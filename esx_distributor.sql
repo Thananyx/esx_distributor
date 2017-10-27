096076f298a8d9a23f95caaa60c59fd60a756294
thananyx <harold_45000@hotmail.com>
2017-10-27 03:09:10 +0200

    first commit

---
+++ esx_distributor.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `distributor` (
+  
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `distributor` (name, item, price) VALUES
+	('Distributor','pepsi',3),
+	('Distributor','',3),
+	('Distributor','',3),
+	('Distributor','',3),
+	('Distributor','',3),
+	('Distributor','',3),
+	('Distributor','',3),
+	('Distributor','',3) -- A vous de mettre d'autres items (le label !!!!!!)
+;
